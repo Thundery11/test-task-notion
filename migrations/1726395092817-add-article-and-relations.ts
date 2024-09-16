7177748b03e2972d47e8ae173b66e4aabdd4c885
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2024-09-16 18:42:26 +0300

    feat: add creating , delete, tags, tag when new chat started

--- migrations/1726395092817-add-article-and-relations.ts
+++ migrations/1726395092817-add-article-and-relations.ts
@@ -1,9 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddArticleAndRelations1726315049471 implements MigrationInterface {
-    name = 'AddArticleAndRelations1726315049471'
+export class AddArticleAndRelations1726395092817 implements MigrationInterface {
+    name = 'AddArticleAndRelations1726395092817'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "login" character varying NOT NULL, "email" character varying NOT NULL, "passwordHash" character varying NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "article" ("id" SERIAL NOT NULL, "title" character varying NOT NULL, "description" character varying NOT NULL, "publicationDate" TIMESTAMP NOT NULL, "authorId" integer NOT NULL, CONSTRAINT "PK_40808690eb7b915046558c0f81b" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "article" ADD CONSTRAINT "FK_a9c5f4ec6cceb1604b4a3c84c87" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
@@ -11,6 +12,7 @@ export class AddArticleAndRelations1726315049471 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "article" DROP CONSTRAINT "FK_a9c5f4ec6cceb1604b4a3c84c87"`);
         await queryRunner.query(`DROP TABLE "article"`);
+        await queryRunner.query(`DROP TABLE "users"`);
     }
 
 }
